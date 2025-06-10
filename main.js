76c1ac99db8115fe9500f9fa32c628cc84df95ec
andreaslarssamils <larssamils.andreas@gmail.com>
2025-06-10 21:49:14 +0200

    fix score display for CPU and initialize variables in main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!');
+const playerButtons = document.querySelectorAll('.player-choices button'); // Selects all buttons inside the player-choices class
+console.log(playerButtons);
+const result = document.querySelector('#result'); // Selects the result element
+console.log(result);
+const CPUChoice = document.querySelector('#cpu-choice'); // Selects the CPU choice element
+console.log(CPUChoice);
+
+const playerScoreDisplay = document.querySelector('#player-score'); // Selects the player score element
+console.log(playerScoreDisplay);
+const CPUChoiceScoreDisplay = document.querySelector('#cpu-score'); // Selects the CPU score element
+console.log(CPUChoiceScoreDisplay);
+
+let playerScore = 0; // Initialize player score
+let CPUScore = 0; // Initialize CPU score
+
+const choices = ['rock', 'paper', 'scissor', 'lizard', 'spock']; // Array of choices
+console.log(choices);
